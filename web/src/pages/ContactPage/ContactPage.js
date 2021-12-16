8948c6afa850f0d31016cb43b73d4f8c19d18071
lssjbrolli <lssjbrolli@gmail.com>
2021-12-16 18:59:28 +0200

    everyones-favorite-thing-to-build-forms

---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,59 @@
+import {
+  FieldError,
+  Form,
+  Label,
+  Submit,
+  TextAreaField,
+  TextField,
+} from '@redwoodjs/forms'
+import { MetaTags } from '@redwoodjs/web'
+
+const ContactPage = () => {
+  return (
+    <>
+      <MetaTags title="Contact" description="Contact page" />
+
+      <Form config={{ mode: 'onBlur' }}>
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /[^@]+@[^.]+\..+/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
+        <Submit>Submit</Submit>
+      </Form>
+    </>
+  )
+}
+
+export default ContactPage
